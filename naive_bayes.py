9d0a20ef90dc4970e0973d82d4690caa4b51210c
Stathis Fotiadis <sfotiadis@gmail.com>
2015-02-13 19:45:23 +0100

    Implementation of naive bayes

--- naive_bayes.py
+++ naive_bayes.py
@@ -1,23 +1,36 @@
 import nltk.classify.util
 from nltk.classify import NaiveBayesClassifier
-from nltk.corpus import movie_reviews
  
-def word_feats(words):
-    return dict([(word, True) for word in words])
- 
-negids = movie_reviews.fileids('neg')
-posids = movie_reviews.fileids('pos')
- 
-negfeats = [(word_feats(movie_reviews.words(fileids=[f])), 'neg') for f in negids]
-posfeats = [(word_feats(movie_reviews.words(fileids=[f])), 'pos') for f in posids]
- 
-negcutoff = len(negfeats)*3/4
-poscutoff = len(posfeats)*3/4
- 
-trainfeats = negfeats[:negcutoff] + posfeats[:poscutoff]
-testfeats = negfeats[negcutoff:] + posfeats[poscutoff:]
-print 'train on %d instances, test on %d instances' % (len(trainfeats), len(testfeats))
- 
-classifier = NaiveBayesClassifier.train(trainfeats)
-print 'accuracy:', nltk.classify.util.accuracy(classifier, testfeats)
-classifier.show_most_informative_features()
+def naive_bayes(pos_samples, neg_samples, ratio):
+    '''Trains a naive bayes classifier with NLTK.
+    Input the positive and negative samples and a given train/test ratio.
+    Returns the classifier and the train/test sets.'''
+    neg_cutoff = int(len(neg_samples) * ratio)
+    pos_cutoff = int(len(pos_samples) * ratio)
+
+    train_samples = neg_samples[:neg_cutoff] + pos_samples[:pos_cutoff]
+    test_samples  = neg_samples[neg_cutoff:] + pos_samples[pos_cutoff:]
+    print 'train on %d instances, test on %d instances' % (len(train_samples), len(test_samples))
+
+    return (NaiveBayesClassifier.train(train_samples), train_samples, test_samples)
+
+def get_classifier_stats(classifier, test_samples):
+    '''Prints statistics of a NLTK classifier'''
+    print 'accuracy:', nltk.classify.util.accuracy(classifier, test_samples)
+    classifier.show_most_informative_features()
+    
+    import collections
+    refsets = collections.defaultdict(set)
+    testsets = collections.defaultdict(set)
+
+    for i, (feats, label) in enumerate(testfeats):
+        refsets[label].add(i)
+        observed = classifier.classify(feats)
+        testsets[observed].add(i)
+
+    print 'pos precision:', nltk.metrics.precision(refsets['pos'], testsets['pos'])
+    print 'pos recall:', nltk.metrics.recall(refsets['pos'], testsets['pos'])
+    print 'pos F-measure:', nltk.metrics.f_measure(refsets['pos'], testsets['pos'])
+    print 'neg precision:', nltk.metrics.precision(refsets['neg'], testsets['neg'])
+    print 'neg recall:', nltk.metrics.recall(refsets['neg'], testsets['neg'])
+    print 'neg F-measure:', nltk.metrics.f_measure(refsets['neg'], testsets['neg'])

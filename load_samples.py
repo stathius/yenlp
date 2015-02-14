e9fc2732b40bc360ad831578f958f2550200003a
Stathis Fotiadis <sfotiadis@gmail.com>
2015-02-14 18:25:10 +0100

    Moving the tokenization inside the filters to allow more flexibility

--- load_samples.py
+++ load_samples.py
@@ -8,20 +8,23 @@ from nltk.tokenize import word_tokenize
 
 template_fn = 'yelp_{category}_reviews_{quantity}_{classif}.json'
 
-def single_words(words):
-    '''Simplest filter that returns all the words of the review.'''
-    return dict([(word, True) for word in words])
+def single_words(text):
+    '''Simplest filter that returns all the words of a given text.'''
+    return dict([(word, True) for word in tokenize(text)])
  
-def stopword_filtered_words(words):
+def stopword_filtered_words(text):
     '''Returns all the single words after removing the stop words.'''
     stopset = set(stopwords.words('english'))
-    return dict([(word, True) for word in words if word not in stopset])
+    return dict([(word, True) for word in tokenize(text) if word not in stopset])
 
-def bigram_words(words, score_fn=BigramAssocMeasures.chi_sq, n=500):
-    ''''''
+def bigram_words(text, score_fn=BigramAssocMeasures.chi_sq, n=500):
+    '''Find the best n bigrams of a text by means of a give measure.'''
+    words = tokenize(text)
     bigram_finder = BigramCollocationFinder.from_words(words)
     bigrams = bigram_finder.nbest(score_fn, n)
     return dict([(ngram, True) for ngram in itertools.chain(words, bigrams)])
+
+def untouched()
 
 def tokenize(text):
     '''Splits a text to words, separates by space and punctuation, 
@@ -36,6 +39,6 @@ def load_samples(category, quantity, filtr):
                                        quantity = quantity, classif = 'pos'), 'r')
     neg_reviews = open(template_fn.format(category = category.lower(), 
                                        quantity = quantity, classif = 'neg'), 'r')
-    pos_samples = [(filtr(tokenize(json.loads(review)['text'])), 'pos') for review in pos_reviews]
-    neg_samples = [(filtr(tokenize(json.loads(review)['text'])), 'neg') for review in neg_reviews]
+    pos_samples = [(filtr((json.loads(review)['text'])), 'pos') for review in pos_reviews]
+    neg_samples = [(filtr((json.loads(review)['text'])), 'neg') for review in neg_reviews]
     return (pos_samples, neg_samples)

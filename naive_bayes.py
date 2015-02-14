49ee7faa641765ad81076ccc912647344e7ffaa8
Stathis Fotiadis <sfotiadis@gmail.com>
2015-02-14 13:54:49 +0100

    Fixed bug in stratified fold creation

--- naive_bayes.py
+++ naive_bayes.py
@@ -1,20 +1,21 @@
 import nltk.classify.util
 from nltk.classify import NaiveBayesClassifier
 from sklearn import cross_validation
+import numpy as np
 
 def naive_bayes(pos_samples, neg_samples, n_folds = 2):
     '''Trains a naive bayes classifier with NLTK. It uses stratified 
     n-fold validation. Inputs are the positive and negative samples and 
     the number of folds. Returns the total accuracy and the classifier and 
     the train/test sets of the last fold.'''
-    samples = pos_samples + neg_samples
+    samples = np.array(pos_samples + neg_samples)
     labels = [label for (words, label) in samples]
     cv = cross_validation.StratifiedKFold(labels, n_folds= n_folds, shuffle=True)
     
     accuracy = 0.0
     for traincv, testcv in cv:
-        train_samples = samples[traincv[0]:traincv[len(traincv)-1]]
-        test_samples = samples[testcv[0]:testcv[len(testcv)-1]]
+        train_samples = samples[traincv]
+        test_samples = samples[testcv]
         classifier = nltk.NaiveBayesClassifier.train(train_samples)
         accuracy += nltk.classify.util.accuracy(classifier, test_samples)
     accuracy /= n_folds
